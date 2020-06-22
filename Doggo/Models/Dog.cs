8030eceb17f9b5f561b2ecb4ebf97fcfaa6a02ee
Brant Pippin <Brant.pippin@gmail.com>
2020-06-22 10:59:48 -0500

    Fully built out the validation branch.

--- Doggo/Models/Dog.cs
+++ Doggo/Models/Dog.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Doggo.Models
 {
     public class Dog
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Hmmm... You should really add a Name...")]
+        [MaxLength(35)]
         public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Owner")]
         public int OwnerId { get; set; }
+
+        [Required]
+        [DisplayName("Breed")]
         public string Breed { get; set; }
+
+        [Required]
+        [DisplayName("Notes")]
         public string Notes { get; set; }
+
+        [Required]
+        [DisplayName("Image Url")]
         public string ImageUrl { get; set; }
     }
 }
--- Doggo/Models/Walker.cs
+++ Doggo/Models/Walker.cs
@@ -1,16 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Doggo.Models
 {
     public class Walker
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Hmmm... You should really add a Name...")]
+        [MaxLength(35)]
         public string Name { get; set; }
-        public int NeighborhoodId { get; set; }
+
+        [Required]
+        [DisplayName("Image Url")]
         public string ImageUrl { get; set; }
+
+        [Required]
+        [DisplayName("Neighborhood")]
+        public int NeighborhoodId { get; set; }
         public Neighborhood Neighborhood { get; set; }
     }
 }
